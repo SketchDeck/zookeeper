6a81964e6435b69bcea6aec26e37975cf0504cb9
David Mack <david@sketchdeck.com>
2016-06-09 16:13:31 -0700

    reset to legacy

--- zk-init.sh
+++ zk-init.sh
@@ -1,29 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
-ZK=$1
-MYID=1
+MYID=$1
+ZK=$2
+
+HOSTNAME=`hostname`
 IPADDRESS=`ip -4 addr show scope global dev eth0 | grep inet | awk '{print \$2}' | cut -d / -f 1`
-
 cd /tmp/zookeeper
+
 if [ -n "$ZK" ] 
 then
-  output=`./bin/zkCli.sh -server $ZK:2181 get /zookeeper/config | grep ^server` 
-  
-  # extract all the zk-ids from the output
-  declare -a id_list=()
-  while read x;
-  do
-    id_list+=(`echo $x | cut -d"=" -f1 | cut -d"." -f2`)
-  done <<<$(output)
-  sorted_id_list=( $(
-    for el in "${id_list[@]}"
-    do  
-      echo "$el"
-    done | sort -n) )
-  
-  # get the next increasing number from the sequence
-  MYID=$((${sorted_id_list[${#sorted_id_list[@]}-1]}+1))
-  echo $output >> /tmp/zookeeper/conf/zoo.cfg.dynamic
+  echo "`bin/zkCli.sh -server $ZK:2181 get /zookeeper/config|grep ^server`" >> /tmp/zookeeper/conf/zoo.cfg.dynamic
   echo "server.$MYID=$IPADDRESS:2888:3888:observer;2181" >> /tmp/zookeeper/conf/zoo.cfg.dynamic
   cp /tmp/zookeeper/conf/zoo.cfg.dynamic /tmp/zookeeper/conf/zoo.cfg.dynamic.org
   /tmp/zookeeper/bin/zkServer-initialize.sh --force --myid=$MYID
@@ -31,8 +17,10 @@ then
   /tmp/zookeeper/bin/zkCli.sh -server $ZK:2181 reconfig -add "server.$MYID=$IPADDRESS:2888:3888:participant;2181"
   /tmp/zookeeper/bin/zkServer.sh stop
   ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /tmp/zookeeper/bin/zkServer.sh start-foreground
+  
 else
   echo "server.$MYID=$IPADDRESS:2888:3888;2181" >> /tmp/zookeeper/conf/zoo.cfg.dynamic
   /tmp/zookeeper/bin/zkServer-initialize.sh --force --myid=$MYID
   ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /tmp/zookeeper/bin/zkServer.sh start-foreground
 fi
+
